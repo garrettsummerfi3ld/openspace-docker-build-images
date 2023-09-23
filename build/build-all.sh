da584bd4f5589d9378c5c547dbb52c790ebb16b1
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2023-09-23 03:06:21 -0500

    Fix broken shell script
    The shell script was broken on Linux/macOS platforms, this is now fixed.

--- build/build-all.sh
+++ build/build-all.sh
@@ -1,4 +1,4 @@
-#!/usr/bin bash
+#!/bin/bash
 # Build all containers in this folder. The tag is "openspace-" followed by the name of
 # the Dockerfile without the file extension
 for f in *.Dockerfile; do
