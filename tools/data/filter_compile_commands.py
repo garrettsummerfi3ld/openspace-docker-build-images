5bf9af4c231f7e7bb62d670140e19b01b34dfb5f
Alexander Bock <alexander.bock@liu.se>
2024-03-11 12:45:42 +0100

    Filter CEF commands

--- tools/data/filter_compile_commands.py
+++ tools/data/filter_compile_commands.py
@@ -2,7 +2,7 @@ import json
 
 """
 This script filters out all of the lines from the compile_commands.json file that belong
-to an external folder. 
+to an external folder.
 """
 
 f = open("compile_commands.json")
@@ -11,24 +11,35 @@ f.close()
 
 res = []
 for i in data:
-  is_ext = "/ext/" in i["directory"]
+  path = ""
+  if "directory" in i:
+    path = i["directory"]
+  elif "file" in i:
+    path = i["file"]
+  else:
+    raise "No directory or file"
+  is_ext = "/ext/" in path
 
-  in_ghoul = "/ext/ghoul/" in i["directory"]
-  in_ghoul_ext = "/ext/ghoul/ext" in i["directory"]
+  in_ghoul = "/ext/ghoul/" in path
+  in_ghoul_ext = "/ext/ghoul/ext" in path
   in_ghoul_not_ext = in_ghoul & (not in_ghoul_ext)
 
-  in_sgct = "/ext/sgct/" in i["directory"]
-  in_sgct_ext = "/ext/sgct/ext" in i["directory"]
+  in_sgct = "/ext/sgct/" in path
+  in_sgct_ext = "/ext/sgct/ext" in path
   in_sgct_not_ext = in_sgct & (not in_sgct_ext)
 
-  in_launcher = "/ext/launcher" in i["directory"]
+  in_launcher = "/ext/launcher" in path
 
+  in_cef_wrapper = "libcef_dll_wrapper" in path
+
+  is_extended_ext = is_ext | in_cef_wrapper
   in_allowed_ext = in_ghoul_not_ext | in_sgct_not_ext | in_launcher
 
-  should_reject = is_ext & (not in_allowed_ext)
+  should_reject = is_extended_ext & (not in_allowed_ext)
 
+  print(f'{path} -> {should_reject}')
   if (not should_reject):
     res.append(i)
 
 f = open("compile_commands.json", "w")
-f.write(json.dumps(res))
+f.write(json.dumps(res, indent=2))
